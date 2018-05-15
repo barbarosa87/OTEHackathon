de79d4c0e8e5d73b9f659b9e89c7fe8fe30a8b6a
elias <elias.barbarosa@gmail.com>
2018-05-15 23:39:02 +0300

    test

---
+++ app/src/main/java/hackathon/ote/gr/otehackathon/activities/SymtpmsListActivity.java
@@ -0,0 +1,32 @@
+package hackathon.ote.gr.otehackathon.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import butterknife.ButterKnife;
+import hackathon.ote.gr.otehackathon.R;
+
+public class SymtpmsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_symtpms_list);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
